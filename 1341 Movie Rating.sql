2ffc46a870a212fde76eb2e14b9dc7755f2515a1
Andrey Kharlamov <dorst.andreas@gmail.com>
2024-12-12 12:11:17 +0300

    Solved task 1527

--- 1341 Movie Rating.sql
+++ 1341 Movie Rating.sql
@@ -27,7 +27,7 @@ Table: MovieRating
 | Column Name   | Type    |
 +---------------+---------+
 | movie_id      | int     |
-;| user_id       | int     |
+| user_id       | int     |
 | rating        | int     |
 | created_at    | date    |
 +---------------+---------+
@@ -42,7 +42,7 @@ Find the name of the user who has rated the greatest number of movies. In case o
 Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
 The result format is in the following example.
 
- 
+
 
 Example 1:
 
@@ -70,7 +70,7 @@ MovieRating table:
 +-------------+--------------+--------------+-------------+
 | 1           | 1            | 3            | 2020-01-12  |
 | 1           | 2            | 4            | 2020-02-11  |
-;| 1           | 3            | 2            | 2020-02-12  |
+| 1           | 3            | 2            | 2020-02-12  |
 | 1           | 4            | 1            | 2020-01-01  |
 | 2           | 1            | 5            | 2020-02-17  | 
 | 2           | 2            | 2            | 2020-02-01  | 
@@ -94,16 +94,16 @@ Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smal
 Create table If Not Exists Movies (movie_id int, title varchar(30));
 Create table If Not Exists Users (user_id int, name varchar(30));
 Create table If Not Exists MovieRating (movie_id int, user_id int, rating int, created_at date);
-Truncate table Movies;
+Delete from Movies;
 insert into Movies (movie_id, title) values ('1', 'Avengers');
 insert into Movies (movie_id, title) values ('2', 'Frozen 2');
 insert into Movies (movie_id, title) values ('3', 'Joker');
-Truncate table Users;
+Delete from Users;
 insert into Users (user_id, name) values ('1', 'Daniel');
 insert into Users (user_id, name) values ('2', 'Monica');
 insert into Users (user_id, name) values ('3', 'Maria');
 insert into Users (user_id, name) values ('4', 'James');
-Truncate table MovieRating;
+Delete from MovieRating;
 insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '1', '3', '2020-01-12');
 insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '2', '4', '2020-02-11');
 insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '3', '2', '2020-02-12');
@@ -113,3 +113,5 @@ insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '2'
 insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '3', '2', '2020-03-01');
 insert into MovieRating (movie_id, user_id, rating, created_at) values ('3', '1', '3', '2020-02-22');
 insert into MovieRating (movie_id, user_id, rating, created_at) values ('3', '2', '4', '2020-02-25');
+
+-- Query
--- 1667 Fix Names in a Table.sql
+++ 1667 Fix Names in a Table.sql
@@ -40,7 +40,7 @@ Output:
 
 -- SQL Schema
 Create table If Not Exists Users (user_id int, name varchar(40));
-Truncate table Users;
+delete from Users;
 insert into Users (user_id, name) values ('1', 'aLice');
 insert into Users (user_id, name) values ('2', 'bOB');
 
